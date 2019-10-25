735c69e22f1fe690b4d4d1e5c728b9e1d88dfeb3
Walter Chang <weihsiu@gmail.com>
2019-10-26 02:14:41 +0800

    initial pool test

--- src/test/scala/stash/util/PoolSpecification.scala
+++ src/test/scala/stash/util/PoolSpecification.scala
@@ -1,8 +1,21 @@
 package stash.util
 
+import cats._
+import cats.effect._
+import cats.effect.implicits._
+import cats.implicits._
 import org.scalacheck._
+import scala.concurrent.ExecutionContext.global
 
 object PoolSpecification extends Properties("Pool") {
   import Prop.forAll
-  property("take/put") = ???
+  implicit val cs = IO.contextShift(global)
+  property("take/put") = forAll(Gen.choose(1, 10)) { n =>
+    val test = for {
+      pool <- Pool.of[IO, Int](List.range(0, n))
+      i <- pool.take
+      _ <- pool.put(i)
+    } yield true
+    test.unsafeRunSync()
+  }
 }
