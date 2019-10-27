f23c3ab3d5aec99841e773ea585e689dc8665454
Walter Chang <weihsiu@gmail.com>
2019-10-28 02:40:48 +0800

    some formatting

--- src/main/scala/stash/kvs/MapKvs.scala
+++ src/main/scala/stash/kvs/MapKvs.scala
@@ -10,7 +10,7 @@ import stash.kvs.Kvs._
 case class MapKvs[F[_]](map: Ref[F, Map[ByteVector, ByteVector]])
 
 object MapKvs {
-  implicit def mapKvs[F[_] : Sync]: Kvs[F, MapKvs[F]] = new Kvs[F, MapKvs[F]] {
+  implicit def mapKvs[F[_]: Sync]: Kvs[F, MapKvs[F]] = new Kvs[F, MapKvs[F]] {
     def insert(x: MapKvs[F], key: ByteVector, value: ByteVector): F[Unit] =
       x.map.update(_ + (key -> value))
     def query(x: MapKvs[F], key: ByteVector): F[Option[ByteVector]] =
@@ -18,5 +18,5 @@ object MapKvs {
     def remove(x: MapKvs[F], key: ByteVector): F[Unit] =
       x.map.update(_ - key)
   }
-  implicit def mapKvsOps[F[_] : Sync](mapKvs: MapKvs[F]): KvsOps[F, MapKvs[F]] = new KvsOps(mapKvs)
+  implicit def mapKvsOps[F[_]: Sync](mapKvs: MapKvs[F]): KvsOps[F, MapKvs[F]] = new KvsOps(mapKvs)
 }
--- src/test/scala/stash/kvs/FileKvsSpecification.scala
+++ src/test/scala/stash/kvs/FileKvsSpecification.scala
@@ -16,18 +16,22 @@ import stash.util.Pool
 
 object FileKvsSpecification extends Properties("FileKvs") {
   import Prop.{forAll, propBoolean}
-  property("insert/query/remove") = forAll { (k: ByteVector, v: ByteVector) => v.nonEmpty ==> {
-    implicit val cs = IO.contextShift(global)
-    val test = for {
-      offsets <- Ref.of[IO, Map[ByteVector, Long]](Map.empty)
-      output <- MVar.of[IO, (Long, OutputStream)]((0, new FileOutputStream("data/test")))
-      inputs <- Pool.of[IO, RandomAccessFile](List.fill(3)(new RandomAccessFile("data/test", "r")))
-      fileKvs = FileKvs(offsets, output, inputs)
-      _ <- fileKvs.insert(k, v)
-      r1 <- fileKvs.query(k)
-      _ <- fileKvs.remove(k)
-      r2 <- fileKvs.query(k)
-    } yield r1 == Some(v) && r2 == None
-    test.unsafeRunSync()
-  }}
+  property("insert/query/remove") = forAll { (k: ByteVector, v: ByteVector) =>
+    v.nonEmpty ==> {
+      implicit val cs = IO.contextShift(global)
+      val test = for {
+        offsets <- Ref.of[IO, Map[ByteVector, Long]](Map.empty)
+        output  <- MVar.of[IO, (Long, OutputStream)]((0, new FileOutputStream("data/test")))
+        inputs <- Pool.of[IO, RandomAccessFile](
+          List.fill(3)(new RandomAccessFile("data/test", "r"))
+        )
+        fileKvs = FileKvs(offsets, output, inputs)
+        _  <- fileKvs.insert(k, v)
+        r1 <- fileKvs.query(k)
+        _  <- fileKvs.remove(k)
+        r2 <- fileKvs.query(k)
+      } yield r1 == Some(v) && r2 == None
+      test.unsafeRunSync()
+    }
+  }
 }
